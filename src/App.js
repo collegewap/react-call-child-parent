bc423860319d02f6d94671d4747e854ede9ae8da
Abhishek EH <collegewap@gmail.com>
2021-09-12 12:08:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, forwardRef, useImperativeHandle } from "react";
+
+const ChildComp = forwardRef((props, ref) => {
+  useImperativeHandle(ref, () => ({
+    showAlert() {
+      alert("Hello from Child Component");
+    },
+  }));
+  return <div></div>;
+});
 
 function App() {
+  const childCompRef = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => childCompRef.current.showAlert()}>Click Me</button>
+      <ChildComp ref={childCompRef} />
     </div>
   );
 }
